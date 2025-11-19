a12e81247f37244940eb595de956fc0b7640ac14
Fathimaksalam <fathimaka411@gmail.com>
2025-11-19 09:28:26 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  
+  return (
+    
+    <div className='text-3xl font-bold text-red-400'>
+      <p>Welcome</p>
+    </div>
+    
+  )
+}
+
+export default App
